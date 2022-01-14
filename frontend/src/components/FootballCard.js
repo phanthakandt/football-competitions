1107804304674a9e0a9d8c2d13a7c01bc14e211e
phanthakandt <phanthakan.d.t@gmail.com>
2022-01-14 15:28:13 +0700

    update responsive player detail page

--- frontend/src/components/FootballCard.js
+++ frontend/src/components/FootballCard.js
@@ -6,7 +6,7 @@ export default function FootballCard(props) {
     return (
         <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 col-xxl-4">
             <a href={"/competition-detail/" + props.competition.id}>
-                <div className="card secondary-card mt-3 flex-box">
+                <div className="card secondary-card mt-3 flex-box mb-3">
                     <h3>
                         <div className="text-one-line">
                             {props.competition.name}
